3a203b8e28a14b152417820cf78a40040fad7de2
Idriss-rayan <rayanidriss27@gmail.com>
2025-07-10 22:56:50 +0600

    ajout

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Utilisateurs')),
+        appBar: AppBar(title: Text('User')),
         body: Padding(
           padding: EdgeInsets.all(16),
           child: Column(
