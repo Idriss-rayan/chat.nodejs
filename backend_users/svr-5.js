ec427b51876a197e608e3cee50016acc2f6d825f
Idriss-rayan <rayanidriss27@gmail.com>
2025-07-17 22:34:09 +0600

    je suis ...

--- backend_users/svr-5.js
+++ backend_users/svr-5.js
@@ -9,6 +9,21 @@ app.use(bodyparser.json());
 
 let contact = [];
 
+app.use((req, res, next) => {
+  const now = new Date();
+  const heure = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  console.log(`${req.method} ${req.path} à ${heure}`);
+  next();
+});
+// Middleware centralisé de gestion d'erreurs
+app.use((err, req, res, next) => {
+  console.error('❌ Erreur attrapée :', err.message);
+
+  res.status(err.status || 500).json({
+    error: true,
+    message: err.message || 'Erreur interne du serveur',
+  });
+});
 
 app.post('/users', (req, res) => {
     const { name, email } = req.body;
@@ -27,7 +42,19 @@ app.post('/users', (req, res) => {
 app.get('/users', (req, res) => {
     res.json(contact);
 });
+app.get('/users/search', (req, res) => {
+  const nameQuery = req.query.name;
 
+  if (!nameQuery) {
+    return res.status(400).json({ message: 'Le paramètre "name" est requis' });
+  }
+
+  const results = contact.filter(user =>
+    user.name.toLowerCase().includes(nameQuery.toLowerCase())
+  );
+
+  res.json(results); // Retourne un tableau vide si aucun résultat
+});
 
 app.delete('/users/:id', (req, res)=>{
     const id = req.params.id;
