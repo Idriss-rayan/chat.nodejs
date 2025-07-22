786a24dc01277fcdc461095b39d211f960658746
Idriss-rayan <rayanidriss27@gmail.com>
2025-07-22 21:41:07 +0600

    add new things

--- backend_users/Event/event1.js
+++ backend_users/Event/event1.js
@@ -1,1 +1,15 @@
 const express = require('express');
+
+const websocket = require('ws');
+const server = new websocket.server({ port: 8080 });
+
+server.on('connection' , (ws) => {
+    console.log('client connecte');
+
+    ws.send('bienvenue');
+
+    ws.on('message', (message) => {
+        console.log('Message recu :', message);
+        ws.send(`recu : ${message}`);
+    });
+});
